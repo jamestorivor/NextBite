755b5a294d225abf35a1077c045916d8744493ca
M Anirudh <manirudh@Ms-MacBook-Pro.local>
2025-06-18 15:26:16 +0530

    Main menu page done

--- client/src/pages/MainMenu.tsx
+++ client/src/pages/MainMenu.tsx
@@ -1,5 +1,22 @@
+import './MainMenu.css';
+
 function MainMenu() {
-  return <h1>Main Menu</h1>;
+  return (
+    <div className="main-menu">
+      <div className="menu-container">
+        <img
+          src="public/images/mainmenu.jpg"
+          alt="Menu"
+          className="menu-image"
+        />
+
+        <button className="btn choose"></button>
+        <button className="btn random"></button>
+        <button className="btn account"></button>
+        <button className="btn settings"></button>
+      </div>
+    </div>
+  );
 }
 
 export default MainMenu;
