a9213d15509ced14bef545db5cdb6e194f0ac63b
M Anirudh <manirudh@192.168.18.36>
2025-06-21 15:04:06 +0800

    ChooseEatery Page updated. One bug remaining: The go button is too tall but I can't get it below 20px so will fix it.

--- client/src/pages/ChooseEatery.tsx
+++ client/src/pages/ChooseEatery.tsx
@@ -1,3 +1,4 @@
+/*
 import './ChooseEatery.css';
 import './MainMenu.css'; 
 
@@ -13,6 +14,123 @@ function ChooseEatery() {
           />
         </div>
         <div className="right-side">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default ChooseEatery;
+*/
+
+import './ChooseEatery.css';
+import './MainMenu.css'; 
+import { useState } from 'react';
+
+function ChooseEatery() {
+  const [inputOne, setInputOne] = useState('');
+  const [inputTwo, setInputTwo] = useState('');
+  const [inputThree, setInputThree] = useState('');
+  const [inputFour, setInputFour] = useState('');
+  const [inputFive, setInputFive] = useState('');
+  const [inputSix, setInputSix] = useState('');
+  const [inputSeven, setInputSeven] = useState('');
+  const [inputEight, setInputEight] = useState('');
+  const [inputNine, setInputNine] = useState('');
+  const [inputTen, setInputTen] = useState('');
+
+
+  return (
+    <div className="main-menu"> 
+      <div className="choose-eatery-page">
+        <div className="left-side">
+          <img
+            src="public/images/chit.jpg"  
+            alt="Chit"
+            className="chit-image"
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter first value"
+            value={inputOne}
+            onChange={(e) => setInputOne(e.target.value)}
+          />
+
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter second value"
+            value={inputTwo}
+            onChange={(e) => setInputTwo(e.target.value)}
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter third value"
+            value={inputThree}
+            onChange={(e) => setInputThree(e.target.value)}
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter fourth value"
+            value={inputFour}
+            onChange={(e) => setInputFour(e.target.value)}
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter fifth value"
+            value={inputFive}
+            onChange={(e) => setInputFive(e.target.value)}
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter sixth value"
+            value={inputSix}
+            onChange={(e) => setInputSix(e.target.value)}
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter seventh value"
+            value={inputSeven}
+            onChange={(e) => setInputSeven(e.target.value)}
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter eighth value"
+            value={inputEight}
+            onChange={(e) => setInputEight(e.target.value)}
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Enter ninth value"
+            value={inputNine}
+            onChange={(e) => setInputNine(e.target.value)}
+          />
+          
+          <img
+            src="public/images/anyother.jpg"
+            alt="Any Other"
+            className="anyother-image"
+          />
+          <input
+            type="text"
+            className="text-box"
+            placeholder="Any other requests?"
+            value={inputTen}
+            onChange={(e) => setInputTen(e.target.value)}
+          />
+          <button className="btn go"></button>
+        </div>
+        <div className="right-side">
+          
         </div>
       </div>
     </div>
