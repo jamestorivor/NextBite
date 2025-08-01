f31ce2e5df7e6fc6133bf9dc9e07f576e5159c0f
marcus <marcustanyonghan@gmail.com>
2025-08-02 00:06:56 +0800

    Cleaned up chooseEatery.py

--- client/src/pages/ChooseEatery.tsx
+++ client/src/pages/ChooseEatery.tsx
@@ -1,22 +1,10 @@
-import Button from "../components/Button";
 import TextBox from "../components/TextBox";
 import "./ChooseEatery.css";
 import "./MainMenu.css";
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-//import { API_BASE_URL } from "../";
 
 function ChooseEatery() {
-  // const [inputOne, setInputOne] = useState("");
-  // const [inputTwo, setInputTwo] = useState("");
-  // const [inputThree, setInputThree] = useState("");
-  // const [inputFour, setInputFour] = useState("");
-  // const [inputFive, setInputFive] = useState("");
-  // const [inputSix, setInputSix] = useState("");
-  // const [inputSeven, setInputSeven] = useState("");
-  // const [inputEight, setInputEight] = useState("");
-  // const [inputNine, setInputNine] = useState("");
-  // const [inputTen, setInputTen] = useState("");
   const navigate = useNavigate();
   const [sortOption, setSortOption] = useState("match");
   const oneBigInput = useRef<HTMLInputElement>(null);
@@ -94,85 +82,6 @@ function ChooseEatery() {
       alert("Something went wrong.");
     }
   };
-
-  // const handleSearch = async () => {
-  //   const keywords = [
-  //     inputOne,
-  //     inputTwo,
-  //     inputThree,
-  //     inputFour,
-  //     inputFive,
-  //     inputSix,
-  //     inputSeven,
-  //     inputEight,
-  //     inputNine,
-  //     inputTen,
-  //   ].filter((word) => word.trim() !== "");
-  //   console.log(keywords);
-
-  //   if (keywords.length === 0) {
-  //     alert("Please enter at least one preference.");
-  //     return;
-  //   }
-
-  //   let userLocation = null;
-
-  //   if (sortOption === "distance") {
-  //     try {
-  //       userLocation = await new Promise((resolve, reject) =>
-  //         navigator.geolocation.getCurrentPosition(
-  //           (position) =>
-  //             resolve({
-  //               lat: position.coords.latitude,
-  //               lng: position.coords.longitude,
-  //             }),
-  //           (error) => reject(error)
-  //         )
-  //       );
-  //     } catch (err) {
-  //       console.error("loc error:", err);
-  //       alert("Please allow location access");
-  //       return;
-  //     }
-  //   }
-
-  //   try {
-  //     console.log("searching");
-  //     const res = await fetch("http://localhost:8080/api/restaurants/search", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify({
-  //         keywords,
-  //         sortOption,
-  //         location: userLocation,
-  //       }),
-  //     });
-  //     console.log("Response status:", res.status);
-  //     if (!res.ok) {
-  //       const errorText = await res.text();
-  //       console.error("Server returned error:", errorText);
-  //       alert("Server error occurred.");
-  //       return;
-  //     }
-
-  //     const data = await res.json();
-  //     console.log("Response data:", data);
-
-  //     if (data.length === 0) {
-  //       alert("No matching restaurants found.");
-  //       return;
-  //     }
-
-  //     const chosen = data;
-  //     /*const chosen = data[Math.floor(Math.random() * data.length)];*/
-  //     console.log(chosen);
-
-  //     navigate("/random", { state: { restaurant: chosen } });
-  //   } catch (err) {
-  //     console.error("Fetch failed:", err);
-  //     alert("Something went wrong.");
-  //   }
-  // };
 
   return (
     <div className="main-menu">
