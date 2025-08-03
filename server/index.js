5389ab79bd0807590491f774c0603053dd26fce5
marcus <marcustanyonghan@gmail.com>
2025-08-03 16:10:03 +0800

    updated index.js file to allow CORS with vercel

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("./db"); 
+const restaurantRouter = require('./routes/restaurants');
 
 const app = express();
 
@@ -17,6 +18,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use("/api/restaurants", restaurantRouter)
 
 app.options("*", cors(corsOptions))
 
